829afe6ad7f4a7136e0a7e7409c65049cc304fbb
PiekharievaK <pechareva@gmail.com>
2025-05-04 11:20:49 +0300

    remove Firebase config file

--- src/middlewares/firebaseAdmin.js
+++ src/middlewares/firebaseAdmin.js
@@ -1,5 +1,7 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../config/firebase.json");
+require('dotenv').config();
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
